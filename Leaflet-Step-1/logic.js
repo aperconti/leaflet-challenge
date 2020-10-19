0a956cb776249ade7ae0ec73d03cc1d18ba07490
Autum Perconti <autumlucille@gmail.com>
2020-10-18 20:19:03 -0700

    adding style.css

---
+++ Leaflet-Step-1/logic.js
@@ -0,0 +1,18 @@
+// Creating map object
+var myMap = L.map("map", {
+    center: [45.523064, -122.676483],
+    zoom: 13
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+// d3.json("https://data.americorps.gov/resource/rs9k-7x53.json").then(Data => {
+    // console.log(Data)
