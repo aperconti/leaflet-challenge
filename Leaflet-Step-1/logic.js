2c2333a6ff53632eae761fc8fbf94a290eb2825b
Autum Perconti <autumlucille@gmail.com>
2020-10-20 22:39:31 -0700

    moving legend

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,8 +1,26 @@
 // Creating map object
 var myMap = L.map("map", {
-    center: [45.523064, -122.676483],
-    zoom: 13
+    center: [37.0902, -95.7129],
+    zoom: 2
 });
+
+const hexColors = [
+    "#c4f069",
+    "#e5f16a",
+    "#f0db66",
+    "#ebbb61",
+    "#e6ab76",
+    "#e17470"
+]
+
+const domain = [
+    10,
+    30,
+    50,
+    70,
+    90,
+    110
+]
 
 // Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -14,5 +32,45 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
 
-// d3.json("https://data.americorps.gov/resource/rs9k-7x53.json").then(Data => {
-    // console.log(Data)
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(Data => {
+    // d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_day.geojson").then(Data => {
+    var features = Data.features;
+
+    // color scale
+    var thresholdScale = d3.scaleThreshold()
+        .domain(domain)
+        .range(hexColors);
+
+    // Loop through data
+    features.forEach(d => {
+        var coordinates = d.geometry.coordinates;
+        var circle = L.circle([coordinates[1], coordinates[0]], {
+            color: "black",
+            weight: .5,
+            fillColor: thresholdScale(coordinates[2]),
+            fillOpacity: 0.8,
+            radius: d.properties.mag * 50000
+        }).addTo(myMap);
+        circle.bindPopup(d.properties.title);
+    });
+
+    var svg = d3.select("svg");
+
+    svg.append("rect")
+        .attr("width", "120")
+        .attr("height", "120")
+        .attr("transform", "translate(0,120)")
+        .attr("fill", "white");
+
+    svg.append("g")
+        .attr("class", "legend")
+        .attr("transform", "translate(10,130)")
+
+    var legend = d3.legendColor()
+        .labelFormat(d3.format(".0f"))
+        .labels(d3.legendHelpers.thresholdLabels)
+        .scale(thresholdScale)
+
+    svg.select(".legend")
+        .call(legend);
+});
